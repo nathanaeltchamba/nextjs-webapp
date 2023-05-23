8e5c399810f11497f5e2b368033a8f90e3665169
nathanaeltchamba <natanshost@gmail.com>
2023-05-23 15:26:54 -0500

    added next-auth

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,19 +9,21 @@ export const metadata = {
 
 const RootLayout = ( {children} ) => {
   return (
-    <html lang='en'>
-        <body>
-            <div className='main'>
-                <div className='gradient'/>
-            </div>
+    <html lang="en">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
 
-            <main className='app'>
-                <Nav />
-                {children}
-            </main>
-        </body>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
     </html>
-  )
+  );
 }
 
 export default RootLayout;
