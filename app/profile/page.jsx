5a7a43202d747aaa0d3fd516df725a04ba392485
nathanaeltchamba <natanshost@gmail.com>
2023-05-25 14:31:00 -0500

    added create post func

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,15 +1,15 @@
 "use client";
 
-import React from 'react'
-import Image from 'next/image'
-import { useSession } from 'next-auth/react'
+import React from 'react';
+import Image from 'next/image';
+import { useSession } from 'next-auth/react';
 
 const page = () => {
 
     const { data: session } = useSession();
   return (
     <div>
-        <h2>Profile Name: {session?.user.name}</h2>
+        <h2>Profile page: {session?.user.name}</h2>
     </div>
   )
 }
