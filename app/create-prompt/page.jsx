69cf351ace9b008d027c6fe7f699df2015cd31af
nathanaeltchamba <natanshost@gmail.com>
2023-05-26 13:42:45 -0500

    create post form added

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,53 +1,52 @@
 "use client";
 
-import { useState} from 'react';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-import Form from '@components/Form';
+import Form from "@components/Form";
 
-const createPrompt = () => {
-  const [submitting, setSubmitting] = useState(false);
-  const [post, setPost] = useState({
-    prompt: '',
-    tag: '',
-  })
+const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const [submitting, setIsSubmitting] = useState(false);
+  const [post, setPost] = useState({ prompt: "", tag: "" });
 
   const createPrompt = async (e) => {
     e.preventDefault();
-    setSubmitting(true);
+    setIsSubmitting(true);
 
     try {
-
-      const response = await fetch('/api/prompt/new', {
-        method: 'POST',
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
         body: JSON.stringify({
           prompt: post.prompt,
           userId: session?.user.id,
-          tag: post.tag
-        })
+          tag: post.tag,
+        }),
+      });
+      console.log(response.body)
 
-      })
-      
       if (response.ok) {
-        router.push('/');
+        router.push("/");
       }
     } catch (error) {
       console.log(error);
-    } finally{
-      setSubmitting(false);
+    } finally {
+      setIsSubmitting(false);
     }
-  }
+  };
 
   return (
-    <Form 
-    type = 'Create'
-    post = { post }
-    setPost = { setPost }
-    submitting = { submitting }
-    handleSubmit = { createPrompt }
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
     />
-  )
-}
+  );
+};
 
-export default createPrompt
+export default CreatePrompt;
